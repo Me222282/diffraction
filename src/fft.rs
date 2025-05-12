bbbbbf8aa57222fbd06c95a92d01a6918bb549ee
Zene <oinkymail3@gmail.com>
2025-05-12 18:38:28 +0100

    iterative fft

--- src/fft.rs
+++ src/fft.rs
@@ -19,15 +19,17 @@ pub fn compute_nth_roots<T: Float + ConstOne + ConstZero + FloatConst>(n: usize)
 
 pub fn dft<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
 {
-    fft_recursive(y.len(), wn, y);
+    fft_recursive(wn, y);
 }
 
-fn fft_recursive<T: Float>(n: usize, wn: &[Complex<T>], y: &mut [Complex<T>])
+/// `y.len()`must be a power of 2
+fn fft_recursive<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
 {
+    let n = y.len();
     if n == 1 { return; }
     let hn = n / 2;
-    fft_recursive(hn, wn, &mut y[0..hn]);
-    fft_recursive(hn, wn, &mut y[hn..n]);
+    fft_recursive(wn, &mut y[0..hn]);
+    fft_recursive(wn, &mut y[hn..n]);
     
     for i in 0..hn
     {
@@ -37,6 +39,33 @@ fn fft_recursive<T: Float>(n: usize, wn: &[Complex<T>], y: &mut [Complex<T>])
         
         let w = wn[i];
         y[i] = y0 + (w * y1);
-        y[i2] = y0 + (w * y1);
+        y[i2] = y0 - (w * y1);
+    }
+}
+
+/// `y.len()`must be a power of 2
+fn fft_iterative<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
+{
+    let n = y.len();
+    let mut s = 2;
+    let mut hs = 1;
+    while s <= n
+    {
+        for i in (0..n).step_by(s)
+        {
+            for j in i..(i + hs)
+            {
+                let j2 = j + hs;
+                let y0 = y[j];
+                let y1 = y[j2];
+                
+                let w = wn[j];
+                y[j] = y0 + (w * y1);
+                y[j2] = y0 - (w * y1);
+            }
+        }
+        
+        s *= 2;
+        hs *= 2;
     }
 }
