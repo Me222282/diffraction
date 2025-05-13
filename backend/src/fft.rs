ff84a2f9a86a58997a2515107513f0cf224cbb75
Zene <oinkymail3@gmail.com>
2025-05-13 15:48:46 +0100

    New iterative method

--- backend/src/fft.rs
+++ backend/src/fft.rs
@@ -43,7 +43,7 @@ pub fn fft_recursive<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
     }
 }
 /// `y.len()`must be a power of 2
-pub unsafe fn fft_recursive_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
+pub fn fft_recursive_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
 {
     let n = y.len();
     if n == 1 { return; }
@@ -53,14 +53,17 @@ pub unsafe fn fft_recursive_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>]
     
     for i in 0..hn
     {
-        let i2 = i + hn;
-        let y0 = y.get_unchecked(i).clone();
-        let y1 = y.get_unchecked(i2).clone();
-        
-        let w = wn.get_unchecked(i);
-        let wy = w * y1;
-        *y.get_unchecked_mut(i) = y0 + wy;
-        *y.get_unchecked_mut(i2) = y0 - wy;
+        unsafe
+        {
+            let i2 = i + hn;
+            let y0 = y.get_unchecked(i).clone();
+            let y1 = y.get_unchecked(i2).clone();
+            
+            let w = wn.get_unchecked(i);
+            let wy = w * y1;
+            *y.get_unchecked_mut(i) = y0 + wy;
+            *y.get_unchecked_mut(i2) = y0 - wy;
+        }
     }
 }
 
@@ -93,7 +96,7 @@ pub fn fft_iterative<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
 }
 
 /// `y.len()`must be a power of 2
-pub unsafe fn fft_iterative_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
+pub fn fft_iterative_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
 {
     let n = y.len();
     let mut s = 2;
@@ -104,19 +107,60 @@ pub unsafe fn fft_iterative_v2<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>]
         {
             for k in 0..hs
             {
-                let j1 = k + i;
-                let j2 = j1 + hs;
-                let y0 = y.get_unchecked(j1).clone();
-                let y1 = y.get_unchecked(j2).clone();
-                
-                let w = wn.get_unchecked(k);
-                let wy = w * y1;
-                *y.get_unchecked_mut(j1) = y0 + wy;
-                *y.get_unchecked_mut(j2) = y0 - wy;
+                unsafe
+                {
+                    let j1 = k + i;
+                    let j2 = j1 + hs;
+                    let y0 = y.get_unchecked(j1).clone();
+                    let y1 = y.get_unchecked(j2).clone();
+                    
+                    let w = wn.get_unchecked(k);
+                    let wy = w * y1;
+                    *y.get_unchecked_mut(j1) = y0 + wy;
+                    *y.get_unchecked_mut(j2) = y0 - wy;
+                }
             }
         }
         
-        s *= 2;
-        hs *= 2;
+        hs = s;
+        s <<= 1;
+    }
+}
+/// `y.len()`must be a power of 2
+pub fn fft_iterative_v3<T: Float>(wn: &[Complex<T>], y: &mut [Complex<T>])
+{
+    let n = y.len();
+    let mut old = 0;
+    let mut current = 2;
+    while current <= n
+    {
+        let x = current & (!old);
+        let mut hs = 1;
+        let mut s = 2;
+        while s <= x
+        {
+            let i = current - s;
+            for k in 0..hs
+            {
+                unsafe
+                {
+                    let j1 = k + i;
+                    let j2 = j1 + hs;
+                    let y0 = y.get_unchecked(j1).clone();
+                    let y1 = y.get_unchecked(j2).clone();
+                    
+                    let w = wn.get_unchecked(k);
+                    let wy = w * y1;
+                    *y.get_unchecked_mut(j1) = y0 + wy;
+                    *y.get_unchecked_mut(j2) = y0 - wy;
+                }
+            }
+            hs = s;
+            // s <<= 1;
+            s *= 2;
+        }
+        
+        old = current;
+        current += 2;
     }
 }
--- benches/fft.rs
+++ benches/fft.rs
@@ -40,7 +40,7 @@ fn it_bench(b: &mut Bencher, data: &(&[Complex<f32>], &[Complex<f32>]))
         let start = Instant::now();
         for _ in 0..iters
         {
-            fft_iterative(data.0, copy.as_mut_slice());
+            fft_iterative_v2(data.0, copy.as_mut_slice());
         }
         return start.elapsed();
     } )
@@ -54,10 +54,7 @@ fn rec2_bench(b: &mut Bencher, data: &(&[Complex<f32>], &[Complex<f32>]))
         let start = Instant::now();
         for _ in 0..iters
         {
-            unsafe
-            {
-                fft_recursive_v2(data.0, copy.as_mut_slice());
-            }
+            fft_recursive_v2(data.0, copy.as_mut_slice());
         }
         return start.elapsed();
     } )
@@ -71,10 +68,7 @@ fn it2_bench(b: &mut Bencher, data: &(&[Complex<f32>], &[Complex<f32>]))
         let start = Instant::now();
         for _ in 0..iters
         {
-            unsafe
-            {
-                fft_iterative_v2(data.0, copy.as_mut_slice());
-            }
+            fft_iterative_v3(data.0, copy.as_mut_slice());
         }
         return start.elapsed();
     } )
@@ -85,20 +79,24 @@ fn bench_all(group: &mut BenchmarkGroup<'_, WallTime>, size: usize)
     let data = gen_data(size);
     let wn = compute_nth_roots::<f32>(size);
     
+    // group.bench_with_input(
+    //     BenchmarkId::new("Recursive", size),
+    //     &(wn.as_ref(), data.as_ref()),
+    //     rec_bench);
+    // group.bench_with_input(
+    //     BenchmarkId::new("Iterative", size),
+    //     &(wn.as_ref(), data.as_ref()),
+    //     it_bench);
+    // group.bench_with_input(
+    //     BenchmarkId::new("Recursive_V2", size),
+    //     &(wn.as_ref(), data.as_ref()),
+    //     rec2_bench);
     group.bench_with_input(
-        BenchmarkId::new("Recursive", size),
-        &(wn.as_ref(), data.as_ref()),
-        rec_bench);
-    group.bench_with_input(
-        BenchmarkId::new("Iterative", size),
+        BenchmarkId::new("Iterative_V2", size),
         &(wn.as_ref(), data.as_ref()),
         it_bench);
     group.bench_with_input(
-        BenchmarkId::new("Recursive_V2", size),
-        &(wn.as_ref(), data.as_ref()),
-        rec2_bench);
-    group.bench_with_input(
-        BenchmarkId::new("Iterative_V2", size),
+        BenchmarkId::new("Iterative_V3", size),
         &(wn.as_ref(), data.as_ref()),
         it2_bench);
 }
@@ -107,10 +105,12 @@ fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("FFT");
     
     // size 8
-    bench_all(&mut group, 8);
-    bench_all(&mut group, 16);
-    bench_all(&mut group, 64);
+    // bench_all(&mut group, 8);
+    // bench_all(&mut group, 16);
+    // bench_all(&mut group, 64);
     bench_all(&mut group, 128);
+    bench_all(&mut group, 1024);
+    bench_all(&mut group, 65536);
     
     group.finish();
 }
